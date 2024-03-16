bc8aa763975a4957f4cf2384a13505454c1af102
ankit_nagwanshi@crgl-thirdparty.com <ankit_nagwanshi@crgl-thirdparty.com>
2024-03-16 08:59:09 +0530

    animation added

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DetailLevel from './DetailLevel';
 import StyleVariations from './StyleVariations';
 import ColorPicker from './ColorPicker';
+import AnimateObject from './AnimateObject';
 
 const Controls = ({ doc, updateSVGImage }) => {
   return (
@@ -14,6 +15,9 @@ const Controls = ({ doc, updateSVGImage }) => {
       </div>
       <div className='flex flex-col border rounded-lg p-5'>
         <ColorPicker doc={doc} updateSVGImage={updateSVGImage} />
+      </div>
+      <div className='flex flex-col border rounded-lg p-5'>
+        <AnimateObject doc={doc} updateSVGImage={updateSVGImage} />
       </div>
     </div>
   );
